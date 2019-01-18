2a87119306df02608845265fd200f85b0f8921bf
korinokage <soulerich@gmail.com>
2019-01-18 14:01:42 +0100

    CMS hobbies test upload image

--- Back/src/Form/HobbiesType.php
+++ Back/src/Form/HobbiesType.php
@@ -6,6 +6,9 @@ use App\Entity\Hobbies;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
 
 class HobbiesType extends AbstractType
 {
@@ -14,7 +17,14 @@ class HobbiesType extends AbstractType
         $builder
             ->add('link')
             ->add('type')
-        ;
+            ->add('imageFile', VichImageType::class, [
+            'required' => false,
+            'allow_delete' => true,
+            'download_label' => true,
+            'download_uri' => true,
+            'image_uri' => true,
+            'imagine_pattern' => null,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
